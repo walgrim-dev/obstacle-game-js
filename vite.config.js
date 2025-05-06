162f2d7c619c1b75d1aef2ea9e6137e33378c9a0
walgrim-dev <ifonir@gmail.com>
2025-05-06 11:41:45 +0200

    fix: dist repo

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 
     build: {
         rollupOptions: {
-            outDir: resolve(__dirname, 'client/dist'),
+            outDir: resolve(__dirname, 'dist'),
             emptyOutDir: true,
             /*
             input: {
